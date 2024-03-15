55a42544f2bf04a7ba0a98f1b288d934f58db58c
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2024-03-15 16:47:16 +0200

    Add swagger documentation to authentication controller

--- src/main/java/com/online/bookstore/controller/AuthenticationController.java
+++ src/main/java/com/online/bookstore/controller/AuthenticationController.java
@@ -3,6 +3,8 @@ package com.online.bookstore.controller;
 import com.online.bookstore.dto.UserRegistrationRequestDto;
 import com.online.bookstore.dto.UserResponseDto;
 import com.online.bookstore.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management", description = "Endpoint for registration and login")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -17,6 +20,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping(value = "/register")
+    @Operation(summary = "Register a new user", description = "Register a new user")
     public UserResponseDto register(@Valid @RequestBody UserRegistrationRequestDto requestDto) {
         return authenticationService.registerUser(requestDto);
     }
