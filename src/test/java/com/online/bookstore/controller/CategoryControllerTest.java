b851796e3dfa21c2fc504529590e601e0ba1c1d4
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2024-03-27 14:42:48 +0200

    Add category controller integration tests

---
+++ src/test/java/com/online/bookstore/controller/CategoryControllerTest.java
@@ -0,0 +1,276 @@
+package com.online.bookstore.controller;
+
+import com.online.bookstore.dto.book.BookWithoutCategoriesDto;
+import com.online.bookstore.dto.category.CategoryDto;
+import com.online.bookstore.dto.category.CreateCategoryRequestDto;
+import com.online.bookstore.exception.dto.ApiError;
+import com.online.bookstore.utils.JacksonUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.online.bookstore.utils.TestDataFactory.CREATE_HISTORICAL_FICTION_CATEGORY;
+import static com.online.bookstore.utils.TestDataFactory.FICTION_CATEGORY;
+import static com.online.bookstore.utils.TestDataFactory.INVALID_NULLABLE_CREATE_CATEGORY_REQUEST;
+import static com.online.bookstore.utils.TestDataFactory.getInvalidCreateCategoryRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Named.named;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(
+        scripts = "/scripts/add-books-with-categories.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS
+)
+@Sql(
+        scripts = "/scripts/remove-books-with-categories.sql",
+        executionPhase = Sql.ExecutionPhase.AFTER_TEST_CLASS
+)
+class CategoryControllerTest {
+    private static final String CATEGORIES_API = "/api/categories";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Test get all categories")
+    @WithMockUser(roles = "USER")
+    void getAllCategories_ReturnsCollectionOfCategories() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get(CATEGORIES_API))
+                .andExpectAll(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final var actual = JacksonUtils.parseJsonList(jsonResponse, CategoryDto.class);
+
+        assertThat(actual).hasSize(10);
+        assertThat(actual).contains(FICTION_CATEGORY);
+    }
+
+    @Test
+    @DisplayName("Test get category by existing ID")
+    @WithMockUser(roles = "USER")
+    void getCategoryById_WithExistingId_Success() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get(CATEGORIES_API + "/1"))
+                .andExpectAll(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final CategoryDto actual = JacksonUtils.parseJson(jsonResponse, CategoryDto.class);
+
+        assertThat(actual).isEqualTo(FICTION_CATEGORY);
+    }
+
+    @Test
+    @DisplayName("Test get category by non-existing ID")
+    @WithMockUser(roles = "USER")
+    void getCategoryById_WithNonExistingId_Failure() throws Exception {
+        mockMvc.perform(get(CATEGORIES_API + "/100"))
+                .andExpectAll(status().is4xxClientError())
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("Test get all books by existing category ID")
+    @WithMockUser(roles = "USER")
+    void getAllBooksByCategoryId_WithExistingId_ReturnsPageWithContents() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get(CATEGORIES_API + "/3/books"))
+                .andExpectAll(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final var actual = JacksonUtils.parseJsonPage(jsonResponse, BookWithoutCategoriesDto.class);
+
+        assertThat(actual.getTotalElements()).isEqualTo(2);
+        assertThat(actual.getNumberOfElements()).isEqualTo(2);
+        assertThat(actual.getContent())
+                .extracting(BookWithoutCategoriesDto::id)
+                .containsExactly(2L, 8L);
+    }
+
+    @Test
+    @DisplayName("Test get all books by non-existing category ID")
+    @WithMockUser(roles = "USER")
+    void getAllBooksByCategoryId_WithNonExistingId_ReturnsEmptyPage() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(get(CATEGORIES_API + "/300/books"))
+                .andExpectAll(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final var actual = JacksonUtils.parseJsonPage(jsonResponse, BookWithoutCategoriesDto.class);
+
+        assertThat(actual.getTotalElements()).isEqualTo(0);
+        assertThat(actual.getNumberOfElements()).isEqualTo(0);
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Test delete category by existing ID")
+    @WithMockUser(roles = "ADMIN")
+    @Rollback
+    void deleteCategory_WithExistingId_Success() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(delete(CATEGORIES_API + "/1"))
+                .andExpectAll(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final CategoryDto actual = JacksonUtils.parseJson(jsonResponse, CategoryDto.class);
+
+        assertThat(actual).usingRecursiveAssertion().isEqualTo(FICTION_CATEGORY);
+    }
+
+    @Test
+    @DisplayName("Test category creation by Admin")
+    @WithMockUser(roles = "ADMIN")
+    @Rollback
+    void createCategory_ValidRequest_Success() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(post(CATEGORIES_API)
+                        .content(JacksonUtils.toJson(CREATE_HISTORICAL_FICTION_CATEGORY))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final CategoryDto actual = JacksonUtils.parseJson(jsonResponse, CategoryDto.class);
+
+        assertThat(actual)
+                .returns(CREATE_HISTORICAL_FICTION_CATEGORY.name(), CategoryDto::name)
+                .returns(CREATE_HISTORICAL_FICTION_CATEGORY.description(), CategoryDto::description)
+                .extracting(CategoryDto::id).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Test update category by Admin")
+    @WithMockUser(roles = "ADMIN")
+    @Rollback
+    void updateCategory_ValidRequest_Success() throws Exception {
+        final MvcResult mvcResult = mockMvc.perform(put(CATEGORIES_API + "/2")
+                        .content(JacksonUtils.toJson(CREATE_HISTORICAL_FICTION_CATEGORY))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        final String jsonResponse = mvcResult.getResponse().getContentAsString();
+        final CategoryDto actual = JacksonUtils.parseJson(jsonResponse, CategoryDto.class);
+
+        assertThat(actual)
+                .returns(CREATE_HISTORICAL_FICTION_CATEGORY.name(), CategoryDto::name)
+                .returns(CREATE_HISTORICAL_FICTION_CATEGORY.description(), CategoryDto::description)
+                .returns(2L, CategoryDto::id);
+    }
+
+    @ParameterizedTest(name = "[{index}] = {0}")
+    @MethodSource(value = "invalidCategoryRequestArgumentsProvider")
+    @DisplayName("Test for invalid request when creating or updating category")
+    @WithMockUser(roles = "ADMIN")
+    void createOrUpdateCategory_InvalidRequest_BadRequest(final CreateCategoryRequestDto invalidReq,
+                                                                 final List<String> expectedErrors)
+            throws Exception {
+        final String invalidJsonRequest = JacksonUtils.toJson(invalidReq);
+        final MvcResult createMvcResult = mockMvc.perform(post(CATEGORIES_API)
+                        .content(invalidJsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().is4xxClientError())
+                .andReturn();
+        final var createJsonResponse = createMvcResult.getResponse().getContentAsString();
+        final var apiErrorCreation = JacksonUtils.parseJson(createJsonResponse, ApiError.class);
+        final MvcResult updateMvcResult = mockMvc.perform(put(CATEGORIES_API + "/1")
+                        .content(invalidJsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(status().is4xxClientError())
+                .andReturn();
+        final var updateJsonResponse = updateMvcResult.getResponse().getContentAsString();
+        final var apiErrorUpdate = JacksonUtils.parseJson(updateJsonResponse, ApiError.class);
+
+        assertThat(apiErrorCreation.errors())
+                .containsExactlyInAnyOrderElementsOf(expectedErrors);
+        assertThat(apiErrorUpdate.errors())
+                .containsExactlyInAnyOrderElementsOf(expectedErrors);
+    }
+
+    @ParameterizedTest(name = "[{index}] = {0}")
+    @MethodSource(value = "securedEndpointsArgumentsProvider")
+    @DisplayName("Test secured endpoints with anonymous user")
+    @WithAnonymousUser
+    void testSecuredEndpoints_WhenAnonymous_ReturnsUnauthenticated(final String path,
+                                                                   final HttpMethod method)
+            throws Exception {
+        mockMvc.perform(request(method, path))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @ParameterizedTest(name = "[{index}] = {0}")
+    @MethodSource(value = "adminEndpointsArgumentsProvider")
+    @DisplayName("Test admin endpoints with user role")
+    @WithMockUser(roles = "USER")
+    void testAdminEndpoints_WithUserRole_ReturnsForbidden(final String path,
+                                                          final HttpMethod method)
+            throws Exception {
+        mockMvc.perform(request(method, path)
+                        .content(JacksonUtils.toJson(CREATE_HISTORICAL_FICTION_CATEGORY))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    private static Stream<Arguments> invalidCategoryRequestArgumentsProvider() {
+        final List<String> expectedNullParameterErrors = List.of(
+                "name must not be null",
+                "description must not be null"
+        );
+        final List<String> expectedInvalidParameterErrors = List.of(
+                "name size must be between 0 and 200",
+                "description size must be between 0 and 10000"
+        );
+        return java.util.stream.Stream.of(
+                Arguments.of(named("Request DTO with nullable fields",
+                        INVALID_NULLABLE_CREATE_CATEGORY_REQUEST), expectedNullParameterErrors),
+                Arguments.of(named("Request DTO with invalid fields",
+                        getInvalidCreateCategoryRequest()), expectedInvalidParameterErrors)
+        );
+    }
+
+    private static Stream<Arguments> securedEndpointsArgumentsProvider() {
+        return java.util.stream.Stream.of(
+                Arguments.of(named("Get all categories",
+                        CATEGORIES_API), HttpMethod.GET),
+                Arguments.of(named("Get category by ID",
+                        CATEGORIES_API + "/1"), HttpMethod.GET),
+                Arguments.of(named("Get all books by category ID",
+                        CATEGORIES_API + "/1/books"), HttpMethod.GET),
+                Arguments.of(named("Create a category",
+                        CATEGORIES_API), HttpMethod.POST),
+                Arguments.of(named("Update category by ID",
+                        CATEGORIES_API + "/1"), HttpMethod.PUT),
+                Arguments.of(named("Delete category by ID",
+                        CATEGORIES_API + "/1"), HttpMethod.DELETE)
+        );
+    }
+
+    private static Stream<Arguments> adminEndpointsArgumentsProvider() {
+        return java.util.stream.Stream.of(
+                Arguments.of(named("Create a category",
+                        CATEGORIES_API), HttpMethod.POST),
+                Arguments.of(named("Update category by ID",
+                        CATEGORIES_API + "/1"), HttpMethod.PUT),
+                Arguments.of(named("Delete category by ID",
+                        CATEGORIES_API + "/1"), HttpMethod.DELETE)
+        );
+    }
+
+}
--- src/test/java/com/online/bookstore/utils/TestDataFactory.java
+++ src/test/java/com/online/bookstore/utils/TestDataFactory.java
@@ -2,6 +2,8 @@ package com.online.bookstore.utils;
 
 import com.online.bookstore.dto.book.BookDto;
 import com.online.bookstore.dto.book.CreateBookRequestDto;
+import com.online.bookstore.dto.category.CategoryDto;
+import com.online.bookstore.dto.category.CreateCategoryRequestDto;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -40,6 +42,22 @@ public final class TestDataFactory {
             null
     );
 
+    public static CategoryDto FICTION_CATEGORY = new CategoryDto(
+            1L,
+            "Fiction",
+            "Works of imaginative narration, often including elements not entirely based on reality."
+    );
+
+    public static CreateCategoryRequestDto CREATE_HISTORICAL_FICTION_CATEGORY = new CreateCategoryRequestDto(
+            "Historical Fiction",
+            "Works of fiction set in the past, often incorporating real historical events, people, or settings into the narrative."
+    );
+
+    public static final CreateCategoryRequestDto INVALID_NULLABLE_CREATE_CATEGORY_REQUEST = new CreateCategoryRequestDto(
+            null,
+            null
+    );
+
     public static CreateBookRequestDto getInvalidCreateBookRequest() {
         final String title = "This title is more than fifty characters long, which violates the size constraint.";
         final String author = "This author name is more than one hundred characters long, which violates the size constraint. This author name is more than one hundred characters long, which violates the size constraint.";
@@ -50,5 +68,11 @@ public final class TestDataFactory {
         final Set<Long> categoryIds = Collections.emptySet();
 
         return new CreateBookRequestDto(title, author, isbn, price, description, coverImage, categoryIds);
+    }
+
+    public static CreateCategoryRequestDto getInvalidCreateCategoryRequest() {
+        final String name = String.join("", Collections.nCopies(4, "This title is more than fifty characters long, which violates the size constraint."));
+        final String description = String.join("", Collections.nCopies(1000, "This description is more than ten thousand characters long, which violates the size constraint."));
+        return new CreateCategoryRequestDto(name, description);
     }
 }
